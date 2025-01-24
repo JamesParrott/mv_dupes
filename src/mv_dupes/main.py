3b36ffc80dc6a0ffbcca2588b7b6416e4085fd5e
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-01-24 12:06:00 +0000

    Initial commit. uv package skeleton.  Checked with Ruff

---
+++ src/mv_dupes/main.py
@@ -0,0 +1,33 @@
+import sys
+import collections
+import hashlib
+import pathlib
+import shutil
+
+
+def main(args=sys.argv[1:]):
+    destination_dir_name = args[0] if args else "EXTRA_DUPLICATES"
+
+    destination_root = pathlib.Path(destination_dir_name)
+
+    file_paths_and_mtimes = collections.defaultdict(list)
+
+    for path in pathlib.Path.cwd().iterdir():
+        if not path.is_file():
+            continue
+        with path.open("rb") as f:
+            digest = hashlib.file_digest(f, "sha256")
+        file_paths_and_mtimes[digest.hexdigest()].append((path, path.stat().st_mtime))
+
+    for hex_digest, files_and_mtimes in file_paths_and_mtimes.items():
+        files_and_mtimes.sort(lambda tuple_: tuple_[1])
+
+        destination = destination_root / hex_digest
+        destination.mkdir(parents=True, exist_ok=True)
+        for i, (file_path, mtime) in enumerate(files_and_mtimes[1:-1], start=1):
+            shutil.move(file_path, destination)
+        print(f"Moved {i} file(s) to {destination}")
+
+
+if __name__ == "__main__":
+    main()
